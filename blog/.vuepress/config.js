294546c4c46358f17c36c169bb243f0c04337515
Noah <twelverie@outlook.com>
2022-03-25 21:29:05 +0800

    feat: init blog and add an article

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: 'North-log', // Title for the site. This will be displayed in the navbar.
+  theme: '@vuepress/theme-blog',
+  themeConfig: {
+    // Please keep looking down to see the available options.
+    footer: {
+      contact: [
+        {
+          type: 'github',
+          link: 'https://github.com/Noahs98',
+        }
+      ],
+      copyright: [
+        {
+          text: 'Copyright © 2021-present Noah Zhang',
+        },
+      ],
+    }
+  }
+}
